f074fa0ccc6e9897a65b6d8fffe7035c188f2452
Yo <yoannbliti@yahoo.fr>
2024-08-01 18:14:57 +0200

    Ajout de la navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
 import HomePage from './HomePage';
 import ForEachAcademy from './ForEachAcademy';
 import FormationDetail from './FormationDetail';
@@ -8,15 +9,16 @@ import FormationDetail from './FormationDetail';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/foreach-academy" element={<ForEachAcademy />} />
-        <Route path="/formations/:id" element={<FormationDetail />} />
-      </Routes>
+      <div>
+        <Navbar /> {/* Inclure Navbar ici pour qu'il s'affiche sur toutes les pages */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/foreach-academy" element={<ForEachAcademy />} />
+          <Route path="/formations/:id" element={<FormationDetail />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
-
-
