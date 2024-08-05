d85f78d5e4334f79ea8e5112b4a2f1618bba0141
Yo <yoannbliti@yahoo.fr>
2024-08-05 19:59:44 +0200

    maintient du footer uniquement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 // src/App.js
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer'; // Importez le composant Footer
-import HomePage from './HomePage';
-import ForEachAcademy from './ForEachAcademy';
-import FormationDetail from './FormationDetail';
+import Footer from './Footer';
+ 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/foreach-academy" element={<ForEachAcademy />} />
-          <Route path="/formations/:id" element={<FormationDetail />} />
-        </Routes>
-        <Footer /> {/* Inclure le Footer ici pour qu'il soit visible sur toutes les pages */}
-      </div>
-    </Router>
+    <div className="App">
+      <Footer />
+    </div>
   );
 }
 
