768d682105c926d252d2a76fa944a1c1ddf3fd2e
Yo <yoannbliti@yahoo.fr>
2024-08-05 20:35:18 +0200

    fixation de la navbar sur la homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import HomePage from './HomePage';
 import ForEachAcademy from './ForEachAcademy';
 import FormationDetail from './FormationDetail';
-
+import './App.css'; 
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar /> 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/foreach-academy" element={<ForEachAcademy />} />
-          <Route path="/formations/:id" element={<FormationDetail />} />
-        </Routes>
+      <div id="root">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/foreach-academy" element={<ForEachAcademy />} />
+            <Route path="/formations/:id" element={<FormationDetail />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
