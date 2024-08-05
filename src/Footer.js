6a1e4a7c6dc07381ce35fb047e408c1b14a046a6
Yo <yoannbliti@yahoo.fr>
2024-08-05 16:49:40 +0200

    finalisation du footer et ajout de logos

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,11 @@
-// src/Footer.js
-import React from 'react';
-import './Footer.css'; // Assurez-vous que le CSS est importé
+import React from "react";
+import "./Footer.css";
+
+import LogoImage from "./assets/images/ForEach_hor_white.png";
+import FacebookIcon from "./assets/images/icons8-facebook-nouveau-100.png";
+import TikTokIcon from "./assets/images/icons8-tic-tac-100.png";
+import InstagramIcon from "./assets/images/icons8-instagram-100.png";
+import LinkedInIcon from "./assets/images/icons8-linkedin-100-2.png";
 
 function Footer() {
   return (
@@ -8,73 +13,49 @@ function Footer() {
       <div className="footer-container">
         <div className="footer-section logo-section">
           <a href="#">
-            <img
-              src="./mon-projet-header/assets/ForEach_hor_white.png"
-              alt="MonLogo"
-              className="footer-logo-image"
-            />
+            <img src={LogoImage} alt="MonLogo" className="footer-logo-image" />
           </a>
         </div>
         <div className="footer-section social-media">
           <h3>Suivez-nous</h3>
-
-          <div className="social-icons-top">
-            <ul className="social-icons">
-              <li>
-                <a
-                  href="https://www.facebook.com/forEachAcademy"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="./mon-projet-header/assets/icons8-facebook-nouveau-150.png"
-                    alt="Facebook"
-                  />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.tiktok.com/@foreach_academy?_t=8oHqzvp6l9M&_r=1"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="./mon-projet-header/assets/icons8-tic-tac-150.png"
-                    alt="TicToc"
-                  />
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="social-icons-bottom">
-            <ul className="social-icons">
-              <li>
-                <a
-                  href="https://www.instagram.com/foreach_academy?igsh=MW1ieGNmdmdmaWRnNg=="
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="./mon-projet-header/assets/icons8-instagram-150.png"
-                    alt="Instagram"
-                  />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/school/foreach-academy/posts/?feedView=all"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="./mon-projet-header/assets/icons8-linkedin-150.png"
-                    alt="LinkedIn"
-                  />
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul className="social-icons">
+            <li>
+              <a
+                href="https://www.facebook.com/forEachAcademy"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={FacebookIcon} alt="Facebook" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.tiktok.com/@foreach_academy?_t=8oHqzvp6l9M&_r=1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={TikTokIcon} alt="TicToc" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/foreach_academy?igsh=MW1ieGNmdmdmaWRnNg=="
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={InstagramIcon} alt="Instagram" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/school/foreach-academy/posts/?feedView=all"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={LinkedInIcon} alt="LinkedIn" />
+              </a>
+            </li>
+          </ul>
         </div>
         <div className="footer-section contact-info">
           <h3>Contact</h3>
