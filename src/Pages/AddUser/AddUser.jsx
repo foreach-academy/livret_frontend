6089ce2b0db1752dcb1e9a2963f12bead1c6c164
brandon <tassinbrandon7@gmail.com>
2024-09-20 16:54:27 +0200

    Ajout du sytéme d'ajout utilisateur dans la BDD (non testé pour l'instant)

--- src/Pages/AddUser/AddUser.jsx
+++ src/Pages/AddUser/AddUser.jsx
@@ -1,51 +1,200 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/AddUser/AddUser.css';
+import UserServices from '../../Services/UserServices';
+import RoleServices from '../../Services/RoleServices';
 
 function AddUser() {
-  return <>
-    <h1 class="title_Pages">Ajouter un utilisateur</h1>
-    <div id='form_add_contener'>
-        <form id='form_add'>
-            <label class="label_add" htmlFor="">First name</label>
+  // États pour les champs du formulaire
+  const [firstName, setFirstName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [promo, setPromo] = useState('');
+  const [roleId, setRoleId] = useState('');
+  const [company, setCompany] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-            <input class="input_add" type="text"  />
+  // État pour les rôles
+  const [roles, setRoles] = useState([]);
 
-            <label id='surname_label_add' class="label_add at_second_label" htmlFor="">Surname</label>
+  // Gestion des erreurs de validation
+  const [errors, setErrors] = useState({});
 
-            <input class="input_add"  type="text"  />
+  // Charger les rôles lors du montage du composant
+  useEffect(() => {
+    const fetchRoles = async () => {
+      try {
+        const response = await RoleServices.fetchAllRole();
+        setRoles(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des rôles:', error);
+      }
+    };
 
-            <label id='email_label_add' class="label_add at_second_label" htmlFor="">Email</label>
+    fetchRoles();
+  }, []);
 
-            <input class="input_add" type="email" />
+  // Fonction pour gérer la soumission du formulaire
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-            <label htmlFor="" id='promo_label_add' class='label_add at_second_label'>Promo</label>
+    const validationErrors = validateForm();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
 
-            <input type="text" class="input_add"/>
+    try {
+      const user = { 
+        firstName, 
+        surname, 
+        email, 
+        promo, 
+        role_id: roleId, 
+        company, 
+        password 
+      };
+      const response = await UserServices.addUser(user);
+      console.log('Utilisateur ajouté avec succès:', response.data);
+      resetForm();
+    } catch (error) {
+      console.error('Erreur lors de l\'ajout de l\'utilisateur:', error);
+    }
+  };
 
-            <label htmlFor="" id='role_label_add' class="label_add at_second_label">Role</label>
+  // Fonction pour valider les champs du formulaire
+  const validateForm = () => {
+    const errors = {};
+    if (!firstName) errors.firstName = 'Le prénom est requis';
+    if (!surname) errors.surname = 'Le nom est requis';
+    if (!email) errors.email = 'L\'email est requis';
+    if (!promo) errors.promo = 'La promotion est requise';
+    if (!company) errors.company = 'L\'entreprise est requise';
+    if (!roleId) errors.role = 'Le rôle est requis';
+    
+    // Validation du mot de passe
+    if (!password) {
+        errors.password = 'Le mot de passe est requis';
+    } else {
+        const passwordRegex = /^(?=.*[A-Z]).{10,}$/;
+        if (!passwordRegex.test(password)) {
+            errors.password = 'Le mot de passe doit contenir au moins 10 caractères et une lettre majuscule';
+        }
+    }
+    
+    if (password !== confirmPassword) errors.confirmPassword = 'Les mots de passe ne correspondent pas';
 
-            <select name="role" id="role_select_add">
-              <option value="Apprenant" default>Apprenant</option>
-              <option value="Formateur">Formateur</option>
-              <option value="Admin">Admin</option>
-            </select>
+    return errors;
+  };
 
-            <label id='company_label_add' class="label_add at_second_label" htmlFor="">Company</label>
+  // Fonction pour réinitialiser les champs du formulaire
+  const resetForm = () => {
+    setFirstName('');
+    setSurname('');
+    setEmail('');
+    setPromo('');
+    setRoleId('');
+    setCompany('');
+    setPassword('');
+    setConfirmPassword('');
+    setErrors({});
+  };
 
-            <input class="input_add" type="text" />
+  return (
+    <>
+      <h1 class="title_Pages">Ajouter un utilisateur</h1>
+      <div id='form_add_contener'>
+        <form id='form_add' onSubmit={handleSubmit}>
+          <label class="label_add" htmlFor="firstName">First name</label>
+          <input
+            class="input_add"
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          {errors.firstName && <span class="error">{errors.firstName}</span>}
 
-            <label id='password_label_add' class="label_add at_second_label" htmlFor="">Password</label>
+          <label id='surname_label_add' class="label_add at_second_label" htmlFor="surname">Surname</label>
+          <input
+            class="input_add"
+            type="text"
+            id="surname"
+            value={surname}
+            onChange={(e) => setSurname(e.target.value)}
+          />
+          {errors.surname && <span class="error">{errors.surname}</span>}
 
-            <input class="input_add" type="password" />
+          <label id='email_label_add' class="label_add at_second_label" htmlFor="email">Email</label>
+          <input
+            class="input_add"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errors.email && <span class="error">{errors.email}</span>}
 
-            <label id='confirm_label_add' class= "at_second_label" htmlFor="">Confirm Password</label>
+          <label htmlFor="promo" id='promo_label_add' class="label_add at_second_label">Promo</label>
+          <input
+            type="text"
+            class="input_add"
+            id="promo"
+            value={promo}
+            onChange={(e) => setPromo(e.target.value)}
+          />
+          {errors.promo && <span class="error">{errors.promo}</span>}
 
-            <input class="input_add" type="password" />
+          <label htmlFor="role" id='role_label_add' class="label_add at_second_label">Role</label>
+          <select
+            name="role"
+            id="role_select_add"
+            value={roleId}
+            onChange={(e) => setRoleId(e.target.value)}
+          >
+            <option value="1">Apprenant</option> {/* Option par défaut */}
+            {roles.map((role) => (
+              role.id !== 1 && (
+                <option key={role.id} value={role.id}>{role.name}</option>
+              )
+            ))}
+          </select>
+          {errors.role && <span className="error">{errors.role}</span>}
 
-            <button id='button_submit_add_user' type='submit'>Ajouter l'utilisateur</button>
+          <label id='company_label_add' class="label_add at_second_label" htmlFor="company">Company</label>
+          <input
+            class="input_add"
+            type="text"
+            id="company"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+          />
+          {errors.company && <span class="error">{errors.company}</span>}
+
+          <label id='password_label_add' class="label_add at_second_label" htmlFor="password">Password</label>
+          <input
+            class="input_add"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors.password && <span class="error">{errors.password}</span>}
+
+          <label id='confirm_label_add' class="label_add at_second_label" htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            class="input_add"
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          {errors.confirmPassword && <span class="error">{errors.confirmPassword}</span>}
+
+          <button id='button_submit_add_user' type='submit'>Ajouter l'utilisateur</button>
         </form>
-    </div>
-  </>
+      </div>
+    </>
+  );
 }
-
-export default AddUser
+export default AddUser;
