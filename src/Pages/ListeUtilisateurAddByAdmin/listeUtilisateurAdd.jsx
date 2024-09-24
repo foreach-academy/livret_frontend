6460ca012b471e89c3ac5ec7a884d88eafe1f391
Mariepo <marie.potte.62@gmail.com>
2024-09-24 11:29:42 +0200

    ajout check token

--- src/Pages/ListeUtilisateurAddByAdmin/listeUtilisateurAdd.jsx
+++ src/Pages/ListeUtilisateurAddByAdmin/listeUtilisateurAdd.jsx
@@ -5,19 +5,20 @@ import UserServices from '../../Services/UserServices';
 const UserList = () => {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    // Fonction pour récupérer tous les utilisateurs
-    const fetchAllUsers = async () => {
-      try {
-        const response = await UserServices.fetchAllUser();
-        console.log(response);
-        setUsers(response.data);
-      } catch (error) {
-        console.error('Erreur lors de la récupération des utilisateurs:',  error.response ? error.response.data : error.message);
-      }
-    };
+  // Fonction pour récupérer tous les utilisateurs
+  const fetchAllUsers = async () => {
+    try {
+      const response = await UserServices.fetchAllUser();
+      setUsers(response.data);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des utilisateurs:', error.response ? error.response.data : error.message);
+    }
+  };
 
-    // Appel initial pour charger les utilisateurs
+  useEffect(() => {
+    UserServices.checkToken();
+    const isAdmin = UserServices.isAdmin(); // Stocke le résultat de isAdmin
+    console.log("L'utilisateur est administrateur :", isAdmin); 
     fetchAllUsers();
 
     // Polling toutes les 50 secondes pour mettre à jour la liste
@@ -41,14 +42,14 @@ const UserList = () => {
         <div className="title_user_list_contener">
           <h2 className="title_user_list">Liste des Formateurs</h2>
           <div className="display_column_block_users">
-            {users.filter(user => user.role.name === 'Formateur').map(user => (
+            {users.filter(user => user.role && user.role.name === 'Formateur').map(user => (
               <div className="block_user" key={user.id}>
                 <div className="info_user">
                   <p>{user.first_name}</p>
                   <p>{user.surname}</p>
                 </div>
                 <button className="button_ChangeRole button_list">
-                  Changer le role
+                  Changer le rôle
                 </button>
               </div>
             ))}
@@ -58,14 +59,14 @@ const UserList = () => {
         <div className="title_user_list_contener">
           <h2 className="title_user_list">Liste des Apprenants</h2>
           <div className="display_column_block_users">
-            {users.filter(user => user.role.name === 'Apprenant').map(user => (
+            {users.filter(user => user.role && user.role.name === 'Apprenant').map(user => (
               <div className="block_user" key={user.id}>
                 <div className="info_user">
                   <p>{user.first_name}</p>
                   <p>{user.surname}</p>
                 </div>
                 <button className="button_ChangeRole button_list">
-                  Changer le role
+                  Changer le rôle
                 </button>
               </div>
             ))}
