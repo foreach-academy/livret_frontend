a5b1701be7a4440232aa513fa244f07ff2b558e5
Mariepo <marie.potte.62@gmail.com>
2024-09-27 10:16:11 +0200

    fix animation

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -16,8 +16,8 @@ const HomePage = () => {
           // Ajouter la classe d'animation correspondante
           if (entry.target.classList.contains('presentation-h1')) {
             entry.target.classList.add('presentation-h1-animation');
-          } else if (entry.target.classList.contains('presentation-p')) {
-            entry.target.classList.add('presentation-p-animation');
+          } else if (entry.target.classList.contains('presentation-section')) {
+            entry.target.classList.add('presentation-section-animation');
           } else if (entry.target.classList.contains('formation-cards')) {
             entry.target.classList.add('formation-cards-animation');
           } else if (entry.target.classList.contains('equipe-member-list')) {
@@ -30,7 +30,7 @@ const HomePage = () => {
     });
     
     observer.observe(document.querySelector('.presentation-h1'));
-    observer.observe(document.querySelector('.presentation-p'));
+    observer.observe(document.querySelector('.presentation-section'));
     observer.observe(document.querySelector('.formation-cards'));
     observer.observe(document.querySelector('.equipe-member-list'));
     observer.observe(document.querySelector('.strategic-info'));
@@ -44,19 +44,21 @@ const HomePage = () => {
       </div>
       <div className="presentation">
         <h1 className="presentation-h1">Présentation de l'organisme</h1>
-        <p className='presentation-p'>
-          À l’origine du projet ForEach Academy, des professionnels du numérique exerçant dans la Région Hauts-de-France, réunis autour de
-          valeurs communes. Depuis des années, cette équipe se retrouve à l’occasion de missions, de sessions de formation ou
-          d’évènements professionnels. Ils échangent régulièrement sur leurs envies de prendre une part active à la formation initiale et
-          permanente dans l’IT, de partager leurs expériences.
-        </p>
-        <p className='presentation-p' id='link_PresentationP'>Ainsi est née <a id="link_presentationA" href="https://www.foreach-academy.fr" target="_blank" rel="noopener noreferrer">ForEach Academy...</a></p>
-        <p className='presentation-p'>
-          Nous formons et accompagnons de nombreux stagiaires dans leurs projets professionnels.
-          Notre rattachement au groupe BAO, dans lequel est également présente l’ESN Symbol IT, nous permet d’intégrer nos offres de 
-          formations dans l’écosystème numérique de la métropole Lilloise et de bénéficier de l’expertise des collaborateurs de cette ESN 
-          dans notre équipe pédagogique.
-        </p>
+        <div className='presentation-section'>
+          <p className='presentation-p'>
+            À l’origine du projet ForEach Academy, des professionnels du numérique exerçant dans la Région Hauts-de-France, réunis autour de
+            valeurs communes. Depuis des années, cette équipe se retrouve à l’occasion de missions, de sessions de formation ou
+            d’évènements professionnels. Ils échangent régulièrement sur leurs envies de prendre une part active à la formation initiale et
+            permanente dans l’IT, de partager leurs expériences.
+          </p>
+          <p className='presentation-p' id='link_PresentationP'>Ainsi est née <a id="link_presentationA" href="https://www.foreach-academy.fr" target="_blank" rel="noopener noreferrer">ForEach Academy...</a></p>
+          <p className='presentation-p'>
+            Nous formons et accompagnons de nombreux stagiaires dans leurs projets professionnels.
+            Notre rattachement au groupe BAO, dans lequel est également présente l’ESN Symbol IT, nous permet d’intégrer nos offres de 
+            formations dans l’écosystème numérique de la métropole Lilloise et de bénéficier de l’expertise des collaborateurs de cette ESN 
+            dans notre équipe pédagogique.
+          </p>
+        </div>
       </div>
       <div className="formations">
         <h2>Nos formations</h2>
