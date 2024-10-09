534e28baacfa31a8b36573dfd90564e0caadfd80
Mariepo <marie.potte.62@gmail.com>
2024-10-09 11:45:07 +0200

    ajout du dropdown année

--- src/pages/LivretPage/LivretPage.jsx
+++ src/pages/LivretPage/LivretPage.jsx
@@ -1,66 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import FormationServices from '../../Services/FormationServices';
-import { useParams } from 'react-router-dom';
-import "../../styles/LivretPage/LivretPage.css"
+import { useParams, useNavigate } from 'react-router-dom'; 
+import "../../styles/LivretPage/LivretPage.css";
 import UserServices from '../../Services/UserServices';
 
-function LivretPage () {
+function LivretPage() {
   const [modules, setModules] = useState([]);
   const [moduleId, setModuleId] = useState(25);
-  const { formationId }= useParams();
-  const [formationName, setFormationName] = useState([]);
+  const { formationId } = useParams(); 
   const [students, setStudents] = useState([]);
-  const [ search, setSearch ] = useState('');
+  const [formationName, setFormationName] = useState('');
+  const [selectedYear, setSelectedYear] = useState("Première année"); 
+  const [dynamicFormationId, setDynamicFormationId] = useState(formationId); 
+  const [showYearDropdown, setShowYearDropdown] = useState(false); 
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate(); 
   const formateurId = UserServices.getUserId();
 
   const fetchModule = async () => {
-    const response = await FormationServices.getModulesByFormationId(formationId);
+    const response = await FormationServices.getModulesByFormationId(dynamicFormationId);
     setModules(response.data.modules);
-  }
-
+  };
 
   const fetchStudents = async () => {
-    const response = await FormationServices.getStudentsEvaluationsByFormationAndModule(formationId, moduleId);
+    const response = await FormationServices.getStudentsEvaluationsByFormationAndModule(dynamicFormationId, moduleId);
     setFormationName(response.data.title);
     setStudents(response.data.apprenants);
-  } 
+  };
 
+  const handleChange = (event) => {
+    const selectedModuleId = Number(event.target.value);
+    setModuleId(selectedModuleId);
+  };
 
-  const handleChange =  (event) => {
-    const selectedModuleId = Number(event.target.value)
-    setModuleId(selectedModuleId)
-  }
+  const handleYearChange = (event) => {
+    const selectedYear = event.target.value;
+    setSelectedYear(selectedYear);
+    const newFormationId = selectedYear === "Première année" ? 2 : 4;
+    setDynamicFormationId(newFormationId);
+    navigate(`/${newFormationId}/concepteur-developpeur-application`);
+  };
 
   const handleSearch = (event) => {
     setSearch(event.target.value);
-  }
+  };
 
   const removeAccent = (string) => {
     return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  }
+  };
 
   const searchedStudents = students.filter((student) => {
-      const firstName = removeAccent(student.first_name).toLowerCase();
-      const lastName = removeAccent(student.surname).toLowerCase();
-      const searchText = removeAccent(search).toLowerCase();
-    return (
-      firstName.includes(searchText) ||
-      lastName.includes(searchText)
-    );
+    const firstName = removeAccent(student.first_name).toLowerCase();
+    const lastName = removeAccent(student.surname).toLowerCase();
+    const searchText = removeAccent(search).toLowerCase();
+    return firstName.includes(searchText) || lastName.includes(searchText);
   });
 
-  const studentsNotEvaluated = students.filter (student => !student.evaluation || student.evaluation.length ===0).length;
-  const selectedModule = modules.find(module => module.id === moduleId);
+  const studentsNotEvaluated = students.filter((student) => !student.evaluation || student.evaluation.length === 0).length;
+  const selectedModule = modules.find((module) => module.id === moduleId);
 
   useEffect(() => {
     fetchModule();
     fetchStudents();
-  }, [moduleId]);
+  }, [dynamicFormationId, moduleId]);
 
   useEffect(() => {
-    fetchStudents();
+    if (formationId === '2' || formationId === '4') {
+      setShowYearDropdown(true);
+    } else {
+      setShowYearDropdown(false); 
+    }
   }, [formationId]);
-
 
   return (
     <div className='formation-container'>
@@ -74,6 +84,15 @@ function LivretPage () {
             ))}
           </select>
         </div>
+        {showYearDropdown && (
+          <div className='formation-filter'>
+            <label htmlFor="year">Année :</label>
+            <select name="year" id="year" onChange={handleYearChange} value={selectedYear}>
+              <option value="Première année">Première année</option>
+              <option value="Deuxième année">Deuxième année</option>
+            </select>
+          </div>
+        )}
         <div className='formation-filter'>
           <label htmlFor="search">Étudiant :</label>
           <div className="search-container">
@@ -82,9 +101,11 @@ function LivretPage () {
           </div>
         </div>
       </div>
+
       <div>
         <h2><span className='badge-primary'>{studentsNotEvaluated}</span> évaluations à compléter</h2>
       </div>
+
       <div>
         <table>
           <thead>
@@ -116,7 +137,7 @@ function LivretPage () {
         </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default LivretPage
+export default LivretPage;
