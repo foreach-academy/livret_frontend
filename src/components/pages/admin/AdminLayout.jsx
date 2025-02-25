9d8213e5f454168bad9335d0bb2e19dea95f0487
TGauquelin <timothe.gauquelin@symbol-it.fr>
2025-02-25 09:44:14 +0100

    :recycle: refacto(): remove all body rules in css & add scroll inside admin structure body

--- src/components/pages/admin/AdminLayout.jsx
+++ src/components/pages/admin/AdminLayout.jsx
@@ -6,13 +6,13 @@ const AdminLayout = ({ children }) => {
   const [headerHeight, setHeaderHeight] = useState(null);
 
   return (
-    <div>
+    <>
       <Header setHeaderHeight={setHeaderHeight}/>
       <div className="d-flex">
         <AdminNavbar headerHeight={headerHeight}/>
-        <div className="w-100 p-3">{children}</div>
+        <div className="w-100 p-3 overflow-auto" style={{height: 'calc(100vh - 80px)'}}>{children}</div>
       </div>
-    </div>
+    </>
   );
 };
 
