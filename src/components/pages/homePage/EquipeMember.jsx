420f578815abb2c7c4d169f72cc5416828bc3d6a
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2025-03-03 15:52:37 +0100

    :recycle: refacto(): correction de la taille des cards

--- src/components/pages/homePage/EquipeMember.jsx
+++ src/components/pages/homePage/EquipeMember.jsx
@@ -5,7 +5,7 @@ import { Card } from 'react-bootstrap';
 
 const EquipeMember = ({ photo, name, position, email }) => {
   return (
-    <Card className="m-4 rounded" style={{ width: '18rem' }}>
+    <Card className="m-4 rounded" style={{width:"15rem", height:"25rem" }}>
     {photo ? <Card.Img variant="top" src={photo} /> : <Card.Img variant="top" src="/images/workers/fe-avatar.png" />}
     <Card.Body className="bg-fe-black-blue rounded-bottom d-flex flex-column align-items-center">
       <Card.Title className="text-white fw-bold ">{name}</Card.Title>
