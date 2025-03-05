19caefdd34ffa5799920f9199e585d1e2f559923
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2025-03-05 09:01:58 +0100

    :recycle: refacto(): réparation du retryTime sur loginPage

--- src/utils/apiClient.jsx
+++ src/utils/apiClient.jsx
@@ -2,21 +2,26 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, 
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error) {
-      console.log(error);
-      const errorMessage = error.response.data.errors[0] || error.message || "Une erreur est survenue.";
-      toast.error(errorMessage);
-    } else {
-      toast.error("Une erreur inconnue s'est produite.");
+    let errorMessage = "Une erreur inconnue s'est produite.";
+    console.log("erreur api :", error)
+    if (error.response && error.response.data) {
+      if (error.response.data.errors && error.response.data.errors.length > 0) {
+        errorMessage = error.response.data.errors[0];
+      } else if (error.response.data.message) {
+        errorMessage = error.response.data.message;
+      }
     }
+
+    toast.error(errorMessage);
     return Promise.reject(error);
   }
 );
