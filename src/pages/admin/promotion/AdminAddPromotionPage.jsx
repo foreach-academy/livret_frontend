2a356bd345fe4ad1eb1c49109a4ba8df75b60e5b
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2025-03-07 14:17:01 +0100

    :recycle: refacto(): selectors fonctionnels

--- src/pages/admin/promotion/AdminAddPromotionPage.jsx
+++ src/pages/admin/promotion/AdminAddPromotionPage.jsx
@@ -12,6 +12,7 @@ import ModulesService from '../../../services/ModulesService';
 import ModulePromotionService from '../../../services/ModulePromotionService.js';
 import Button from '../../../components/shared/Button'
 import { toast } from 'react-toastify';
+import AdminBodyTitle from '../../../components/shared/AdminBodyTitle.jsx';
 
 function AdminAddPromotionPage() {
     const navigate = useNavigate();
@@ -45,18 +46,26 @@ function AdminAddPromotionPage() {
 
     const handleUserSelection = (role, userId) => {
         if (!userId) return;
+
+        const selectedUser = users.find(user => user.id === userId);
+        if (!selectedUser) return;
+
+        const newEntry = { id: userId, userName: `${selectedUser.firstname} ${selectedUser.lastname}` };
+
         setPromotion((prev) => ({
             ...prev,
-            [role]: prev[role].includes(userId) ? prev[role] : [...prev[role], userId],
+            [role]: prev[role].some(user => user.id === userId) ? prev[role] : [...prev[role], newEntry],
         }));
     };
+
 
     const handleUserRemoval = (role, userId) => {
         setPromotion((prev) => ({
             ...prev,
-            [role]: prev[role].filter(id => id !== userId),
+            [role]: prev[role].filter(user => user.id !== userId),
         }));
     };
+
 
     const handleModuleChange = (moduleId, field, value) => {
         setPromotion((prev) => ({
@@ -66,94 +75,82 @@ function AdminAddPromotionPage() {
     };
 
     const handleSubmit = async () => {
-        if (!promotion.title || !promotion.training_id) {
-            toast.error("Veuillez renseigner tous les champs.");
-            return;
-        }
-
-        if (promotion.students.length === 0 || promotion.trainers.length === 0) {
-            toast.error("Veuillez sélectionner au moins un étudiant et un formateur.");
+        if (!promotion.title) {
+            toast.error("Veuillez renseigner un nom de promotion.", { className: "toast-error", });
             return;
         }
 
         try {
-
-            const newPromotion = await PromotionsService.addPromotion(promotion);
-            
-            
-
+            await PromotionsService.addPromotion(promotion);
             navigate(FRONT_ADMIN_PROMOTION);
         } catch (error) {
             console.error("Erreur lors de l'ajout de la promotion ou des modules:", error);
+            throw (error);
+
         }
     };
 
     return (
         <AdminLayout>
-            <div className="container-admin">
-                <h1>Ajouter une promotion</h1>
-                <div className="form_blue_contener wider">
-                    <div className="form_blue gap-3">
-                        <div className='d-flex flex-row gap-3'>
-                            <SelectInputGeneric
-                                label="Choisissez une formation"
-                                options={trainings}
-                                selectedValue={promotion.training_id}
-                                onChange={(e) => setPromotion({ ...promotion, training_id: e.target.value })}
-                                getOptionLabel={(training) => training.title}
+            <AdminBodyTitle pageTitle="Ajouter une promotion" />
+            <div className="form_blue_contener wider">
+                <div className="form_blue gap-3">
+                    <div className='d-flex flex-row gap-3'>
+                        <SelectInputGeneric
+                            label="Choisissez une formation"
+                            options={trainings}
+                            selectedValue={promotion.training_id}
+                            onChange={(e) => setPromotion({ ...promotion, training_id: e.target.value })}
+                            getOptionLabel={(training) => training.title}
+                        />
+                        <Input
+                            changeFunction={(e) => setPromotion({ ...promotion, title: e.target.value })}
+                            labelName="Nom de la promotion :"
+                            type="text"
+                            value={promotion.title}
+                            className="color-black-text"
+                        />
+                    </div>
+                    {listSelector.map(({ label, role, options }, index) => (
+                        <SelectInputGeneric
+                            key={index}
+                            label={label}
+                            options={options}
+                            onChange={(e) => handleUserSelection(role, Number(e.target.value))}
+                            selectedItems={promotion[role].map(user => user.id)}
+                            onRemove={(id) => handleUserRemoval(role, id)}
+                            showSelectedList={true}
+                            getOptionLabel={(user) => `${user.firstname} ${user.lastname}`}
+                        />
+                    ))}
+
+                    <h2>Modules</h2>
+                    {promotion.modules.map(module => (
+                        <div key={module.id} className="d-flex gap-3 align-items-center">
+                            <span>{module.title}</span>
+                            <Input
+                                labelName="Date de début"
+                                type="date"
+                                value={module.startDate || ""}
+                                changeFunction={(e) => handleModuleChange(module.id, "startDate", e.target.value)}
                             />
                             <Input
-                                changeFunction={(e) => setPromotion({ ...promotion, title: e.target.value })}
-                                labelName="Nom de la promotion :"
-                                type="text"
-                                value={promotion.title}
-                                className="color-black-text"
+                                labelName="Date de fin"
+                                type="date"
+                                value={module.endDate || ""}
+                                changeFunction={(e) => handleModuleChange(module.id, "endDate", e.target.value)}
+                            />
+                            <SelectInputGeneric
+                                label="Formateur"
+                                options={promotion.trainers}
+                                onChange={(e) => {
+                                    handleModuleChange(module.id, "trainerId", e.target.value);
+                                }}
+                                getOptionLabel={(trainer) => trainer.userName}
                             />
                         </div>
-                        {listSelector.map(({ label, role, options }, index) => (
-                            <SelectInputGeneric
-                                key={index}
-                                label={label}
-                                options={options}
-                                onChange={(e) => handleUserSelection(role, Number(e.target.value))}
-                                selectedItems={promotion[role]}
-                                onRemove={(id) => handleUserRemoval(role, id)}
-                                showSelectedList={true}
-                                getOptionLabel={(user) => `${user.firstname} ${user.lastname}`}
-                            />
-                        ))}
-
-                        <h2>Modules</h2>
-                        {promotion.modules.map(module => (
-                            <div key={module.id} className="d-flex gap-3 align-items-center">
-                                <span>{module.title}</span>
-                                <Input
-                                    labelName="Date de début"
-                                    type="date"
-                                    value={module.startDate || ""}
-                                    changeFunction={(e) => handleModuleChange(module.id, "startDate", e.target.value)}
-                                />
-                                <Input
-                                    labelName="Date de fin"
-                                    type="date"
-                                    value={module.endDate || ""}
-                                    changeFunction={(e) => handleModuleChange(module.id, "endDate", e.target.value)}
-                                />
-                                <SelectInputGeneric
-                                    label="Formateur"
-                                    options={promotion.trainers.map(trainerId => {
-                                        const trainer = users.find(user => user.id === trainerId);
-                                        return trainer ? { value: trainer.id, label: `${trainer.firstname} ${trainer.lastname}` } : null;
-                                    }).filter(Boolean)}
-                                    selectedValue={module.trainerId || ""}
-                                    onChange={(selectedOption) => handleModuleChange(module.id, "trainerId", selectedOption.value)}
-                                    getOptionLabel={(trainer) => trainer.label}
-                                />
-
-                            </div>
-                        ))}
-                        <Button buttonTitle="Ajouter la promotion" className="bg-fe-orange" setAction={handleSubmit} />
-                    </div>
+                    ))}
+                    <Button buttonTitle="Ajouter la promotion" className="bg-fe-orange" setAction={handleSubmit} />
                 </div>
             </div>
         </AdminLayout>
