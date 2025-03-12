f633e70c025cf18aa79187f042363d5d7db599d5
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2025-03-12 11:19:17 +0100

    :sparkles: feat(): formulaire email & formulaire new pwd

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from '../components/shared/navbar/Header'
+import Input from '../components/shared/form/Input';
+import Button from '../components/shared/Button';
+import AuthenticateService from '../services/AuthenticateServices';
+import { useNavigate } from 'react-router-dom';
 
 function ForgetPassword() {
+  const [headerHeight, setHeaderHeight] = useState(null);
+  const [user, setUser] = useState({ email: "" });
+  const navigate = useNavigate()
+
+  const forgetMail = async (e) => {
+    e.preventDefault();
+    await AuthenticateService.sendForgotPasswordMail(user, navigate)
+  }
   return (
-    <div>ForgetPassword</div>
+    <>
+      <Header setHeaderHeight={setHeaderHeight} />
+      <div className="d-flex justify-content-center flex-column align-items-center mt-5 form_blue_contener bg-fe-blue">
+        <span className='d-flex mb-2'>Pour générer votre nouveau mot de passe, veuillez renseigner votre adresse mail</span>
+        <Input
+          labelName="Email"
+          type="email"
+          value={user.email}
+          changeFunction={(e) => setUser((prevState) => ({ ...prevState, email: e.target.value }))}
+          className="color-black-text align-items-center"
+          required={true}
+        />
+        <Button type="form" buttonTitle="Mot de passe oublié" setAction={(e) => forgetMail(e)} className="bg-fe-orange" />
+      </div>
+    </>
   )
 }
 
